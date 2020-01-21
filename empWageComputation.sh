65ec86bb9820ee73659611d42f926b26b06e61b2
shraddha <shraddhat22m@gmail.com>
2020-01-21 18:47:53 +0530

    MODIFIED: Calculating wages for a month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,16 +1,26 @@
 #!/bin/bash -x
 echo "Welcome to Employee Wage"
-PART_TIME_HOURS=4
 WAGE_PER_HOURS=20
-FULL_DAY_HOURS=8
-Employee_check=$((RANDOM%2))
-case $Employee_check in
-		0)
-			echo "PartTimeWages = $(($WAGE_PER_HOURS * $PART_TIME_HOURS))"
+
+PartTime=1
+FullTime=2
+TotalSalary=0
+WorkingDays=20
+for ((i=1; i<=$WorkingDays; i++ ))
+do
+	Employee_check=$((RANDOM%3))
+		case $Employee_check in
+		$PartTime)
+				Employee_Hours=4
+				
 			;;
-		1)
-			echo "FullTimeWages = $(($WAGE_PER_HOURS * $FULL_DAY_HOURS))"
+		$FullTime)
+				Employee_Hours=8
 			;;
 		*)
-			echo "Please enter valid number"
+				Employee_Hours=0
 esac
+
+## calculating employee part time and full time wages##  
+Wages_for_Month=$(($Employee_Hours * $WAGE_PER_HOURS))
+done
