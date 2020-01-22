bc06c7c187d571efa3e7794bebcd366dbd2ca64e
shraddha <shraddhat22m@gmail.com>
2020-01-22 12:51:37 +0530

    MODIFIED: Store the daily wage along with monthly wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash -x 
 echo "Welcome to Employee Wage"
 WAGE_PER_HOURS=20
 HOURS_IN_MONTH=100
@@ -7,31 +7,45 @@ HOURS_IN_MONTH=100
 totalHours=0
 workingDays=0
 function  getEmployeeWorkingHours()
-{		
+{
 		case $1 in
 		0)
 				Employee_Working_Hours=4
-				((workingDays++))			
 				;;
 		1)
 				Employee_Working_Hours=8
-				((workingDays++))
 				;;
-		2)
+		*)
 				Employee_Working_Hours=0
 				;;
 		esac
 		echo $Employee_Working_Hours
+}
+
+#fuction to calculate daily wages
+function getcalculateDailyWage()
+{
+	local WorkHrs=$1
+	EmployeeWages=$(($Employee_Working_Hours * $WAGE_PER_HOURS))
+	echo $EmployeeWages
 
 }
 
 while [[ $totalHours -lt $HOURS_IN_MONTH && $workingDays -lt $WAGE_PER_HOURS  ]]
 do
 		#using function  generate random number#
-		Employee_Working_Hours=$( getEmployeeWorkingHours $((RANDOM%3)) )
+		Employee_Working_Hours=$( getEmployeeWorkingHours $((RANDOM%2)) )
 		#calculating total employees working hours#  
 		totalHours=$((totalHours + Employee_Working_Hours))
+		
+		#calculating  employee daily wages#
+		EmployeeDailyWages[$workingDays]=$( getcalculateDailyWage $Employee_Working_Hours )
+		
+		#calculating total salary#
+		totalSalary=$((totalSalary + ${EmployeeDailyWages[$workingDays]} ))
+		echo  "days = $workingDays "
+		((workingDays++))
 done 
-totalSalary=$((totalHours * WAGE_PER_HOURS))
 
-
+echo "Employee Daily Wages:${EmployeeDailyWages[@]}"
+echo "Employee Monthly Salary:$totalSalary"
